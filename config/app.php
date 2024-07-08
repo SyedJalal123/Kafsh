c572ec8d9e535d761d4dd5a72a3f32689acfb10f
SyedJalal123 <syedjalalshah2004@gmail.com>
2024-07-08 14:28:23 +0500

    8-July

--- config/app.php
+++ config/app.php
@@ -139,6 +139,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        CyrildeWit\EloquentViewable\EloquentViewableServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
