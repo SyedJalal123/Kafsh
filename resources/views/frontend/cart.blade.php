e6e612be31ca23ff53e5fc85aba686dcbc4c1d11
SyedJalal123 <syedjalalshah2004@gmail.com>
2024-07-24 17:13:10 +0500

    ..

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -114,12 +114,12 @@
                                                 <div class="product-details d-flex flex-column">
                                                     <a href="#" class="product-name mb-2">{{$product_data->title}}</a>
                                                     <span class="product-variation">{{$product['variation_title']}}: <strong>{{$product['variation_value']}}</strong></span>
-                                                    <a href="{{url('remove_from_cart')}}/{{$id}}/{{$product['variation_value']}}/close" class="py-1 cart-box-delete">
+                                                    {{-- <a href="{{url('remove_from_cart')}}/{{$id}}/{{$product['variation_value']}}/close" class="py-1 cart-box-delete">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#e60000" class="bi bi-trash" viewBox="0 0 16 16">
                                                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                                             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                                                         </svg>
-                                                    </a>
+                                                    </a> --}}
                                                 </div>
                                             </div>
                                             <div class="w-pc-25 p-0 cart-price price-box cart-price-box">
